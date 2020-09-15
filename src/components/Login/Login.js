5cbf1fa04489f2dabbb487902ca3e904d406b372
Anisur-Rahman-Rakib <amianis1234567890@gmail.com>
2020-09-15 14:01:42 +0600

    n

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -58,6 +58,7 @@ console.log(displayName, email,photoURL,'goooooooooo');
       var token = result.credential.accessToken;
       // The signed-in user info.
       var user = result.user;
+      
       console.log('fb userrrr', user);
       // ...
     }).catch(function(error) {
